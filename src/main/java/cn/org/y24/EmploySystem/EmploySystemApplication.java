bec8bb3f27be53f74e3857df4fb4408ace8a17f2
Y24 <githuby24@gmail.com>
2019-07-08 14:11:42 +0800

    Add Mysql support.

---
+++ src/main/java/cn/org/y24/EmploySystem/EmploySystemApplication.java
@@ -0,0 +1,13 @@
+package cn.org.y24.EmploySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/org/y24/EmploySystem/EmploySystemApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.org.y24.EmploySystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmploySystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
